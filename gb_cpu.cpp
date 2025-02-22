d583d034b95dad80cdce912cfc596d24f70f6712
Cole Palmore <colepalmore2@gmail.com>
2025-02-21 18:06:02 -0600

    updated errores with memory and video_memory sizes being type

--- gb_cpu.cpp
+++ gb_cpu.cpp
@@ -4,8 +4,8 @@
 const short RESO_WIDTH = 160;
 const short RESO_HEIGHT = 144;
 
-const byte VIDEO_MEM_SIZE = 8192;
-const byte MEM_SIZE = 8192;
+const int VIDEO_MEM_SIZE = 8192;
+const int MEM_SIZE = 8192;
 
 const byte BANK_SIZE = 16;
 // the ID of a tile can be found using the following algo adress / 16 mod 256
@@ -22,11 +22,16 @@ struct CPU{
 
   // 16-bit Registers
   
-
-
-  short 
   byte memeory[MEM_SIZE]; // 8 Kib of mem
   byte video_memeory[VIDEO_MEM_SIZE];
 };
 
-CPU cpu;
+void initCPU(){
+  std::cout << "CPU is being intialized..." << '\n';
+  CPU cpu;
+}
+
+int main(){
+  initCPU();
+  return 0;
+}
